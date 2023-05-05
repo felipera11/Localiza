230fd57557c1d5e0be381df047639fdb3133d018
felipera11 <105614828+felipera11@users.noreply.github.com>
2023-05-05 15:21:28 -0300

    refactored main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,93 +1,121 @@
-
 #include <Arduino.h>
 #include <WiFi.h>
 #include <Firebase_ESP_Client.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <Sensitive_data.h>
-
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEScan.h>
 #include <BLEAdvertisedDevice.h>
-
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 
-// ssid e senha do wifi
 #define WIFI_SSID SensitiveData::wifi_ssid
 #define WIFI_PASSWORD SensitiveData::wifi_password
-
-// api do projeto do firebase
 #define API_KEY SensitiveData::firebase_api_key
-
-// url do realtime database
 #define DATABASE_URL SensitiveData::firebase_database_url
+#define scanTime 5
 
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 
-unsigned long sendDataPrevMillis = 0;
-int count = 0;
-bool signupOK = false;
-
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
 
-unsigned long time_now;
-String path_with_time;
-
-std::string scanner_mac_address;
-
-int scanTime = 5; // In seconds
 BLEScan *pBLEScan;
 BLEAdvertisedDevice actualDevice;
 BLEScanResults scanResults;
 
-std::string path_upload_firebase;
+unsigned long time_now;
+unsigned long sendDataPrevMillis = 0;
+
+std::string scanner_mac_address;
 std::string beacon_address;
 
-void setup()
+void firebase_upload(std::string beacon_address, int beacon_rssi)
 {
-  Serial.begin(115200);
+  std::string path_upload_firebase;
+
+  path_upload_firebase = "localiza/beacons/" + beacon_address + "/" + scanner_mac_address + "/rssi";
+  Firebase.RTDB.setInt(&fbdo, path_upload_firebase, beacon_rssi) ? Serial.println("Valor rssi subiu ok no path: " + fbdo.dataPath()) : Serial.println("Deu erro: " + fbdo.errorReason());
+
+  path_upload_firebase = "localiza/beacons/" + beacon_address + "/" + scanner_mac_address + "/time";
+  Firebase.RTDB.setInt(&fbdo, path_upload_firebase, time_now) ? Serial.println("Valor time subiu ok no path: " + fbdo.dataPath()) : Serial.println("Deu erro: " + fbdo.errorReason());
+}
+
+void wifi_connect()
+{
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to Wi-Fi");
+  Serial.println("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED)
   {
-    Serial.print(".");
     delay(300);
   }
-  Serial.println();
   Serial.print("Connected with IP: ");
   Serial.println(WiFi.localIP());
-  Serial.println();
 
   scanner_mac_address = WiFi.macAddress().c_str();
+
   Serial.print("MAC Address: ");
   Serial.println(scanner_mac_address.c_str());
+}
+
+void firebase_connect()
+{
 
   config.api_key = API_KEY;
-
   config.database_url = DATABASE_URL;
 
   if (Firebase.signUp(&config, &auth, "", ""))
   {
-    Serial.println("Conectado ao Firebase");
-    signupOK = true;
+    Serial.println("Firebase connection successful!");
+    Firebase.begin(&config, &auth);
+    Firebase.reconnectWiFi(true);
   }
   else
   {
     Serial.printf("%s\n", config.signer.signupError.message.c_str());
+    Serial.println("Firebase connection failed!");
+    ESP.restart();
   }
+}
 
-  config.token_status_callback = tokenStatusCallback;
+void ble_scan()
+{
+  int count;
+  int beacon_rssi;
 
-  Firebase.begin(&config, &auth);
-  Firebase.reconnectWiFi(true);
+  Serial.println("Scanning...");
+  pBLEScan->start(scanTime, false);
+  Serial.println("Scan done!");
+  scanResults = pBLEScan->getResults();
+  count = scanResults.getCount();
+  for (int i = 0; i < count; i++)
+  {
+
+    Serial.print("Device ");
+    actualDevice = scanResults.getDevice(i);
+    beacon_address = actualDevice.getAddress().toString();
+    beacon_rssi = actualDevice.getRSSI();
+    Serial.println(beacon_address.c_str());
+
+    firebase_upload(beacon_address, beacon_rssi);
+  }
+  pBLEScan->clearResults();
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  wifi_connect();
+
+  firebase_connect();
 
   timeClient.begin();
-  timeClient.setTimeOffset(0); // configura o fuso horario para -3 horas
+  timeClient.setTimeOffset(0);
 
   BLEDevice::init("");
   pBLEScan = BLEDevice::getScan();
@@ -98,66 +126,20 @@ void setup()
 
 void loop()
 {
-
-  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))
+  if (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)
   {
-    sendDataPrevMillis = millis();
-
-    timeClient.forceUpdate();             // atualiza o tempo do servidor NTP
-    time_now = timeClient.getEpochTime(); // retira só o tempo da string
-    Serial.print("Time: ");
-    Serial.println(time_now);
-
-    pBLEScan->start(scanTime, false);
-    Serial.println("Scan done!");
-    scanResults = pBLEScan->getResults();
-    int count = scanResults.getCount();
-    for (int i = 0; i < count; i++)
+    if (Firebase.ready())
     {
+      sendDataPrevMillis = millis();
 
-      Serial.print("Device n");
-      Serial.println(i + 1);
-      actualDevice = scanResults.getDevice(i);
-      beacon_address = actualDevice.getAddress().toString();
-      int beacon_rssi = actualDevice.getRSSI();
-      Serial.println(beacon_address.c_str());
-      Serial.print("RSSI: ");
-      Serial.println(beacon_rssi);
+      timeClient.forceUpdate();
+      time_now = timeClient.getEpochTime();
+      Serial.print("Time: ");
+      Serial.println(time_now);
 
-      path_upload_firebase = "localiza/beacons/" + beacon_address + "/" + scanner_mac_address + "/rssi";
+      ble_scan();
 
-      if (Firebase.RTDB.setInt(&fbdo, path_upload_firebase, beacon_rssi))
-      {
-        Serial.println("PASSED");
-        Serial.print("PATH: ");
-        Serial.println(fbdo.dataPath());
-        Serial.print("TYPE: ");
-        Serial.println(fbdo.dataType());
-      }
-      else
-      {
-        Serial.println("FAILED");
-        Serial.print("REASON: ");
-        Serial.println(fbdo.errorReason());
-      }
-
-      path_upload_firebase = "localiza/beacons/" + beacon_address + "/" + scanner_mac_address + "/time";
-      if (Firebase.RTDB.setInt(&fbdo, path_upload_firebase, time_now))
-      {
-        Serial.println("PASSED");
-        Serial.print("PATH: ");
-        Serial.println(fbdo.dataPath());
-        Serial.print("TYPE: ");
-        Serial.println(fbdo.dataType());
-      }
-      else
-      {
-        Serial.println("FAILED");
-        Serial.print("REASON: ");
-        Serial.println(fbdo.errorReason());
-      }
+      Serial.println("---------------------------------");
     }
-    pBLEScan->clearResults();
-    Serial.println("---------------------------------");
   }
 }
