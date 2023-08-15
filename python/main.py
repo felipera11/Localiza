4d427ff477d37de614a6ae6c748dfc5d5d018a0b
MarcosHenriqueBarbosaPereira <marcos.h@gec.inatel.br>
2023-08-15 14:24:49 -0300

    filter_synchronization

--- python/main.py
+++ python/main.py
@@ -15,8 +15,8 @@ from googleapiclient.errors import HttpError
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1IMZrcDvgddjarKStUBvvv3S-6NIrVxBSd_fl4yAMP2g'
-SAMPLE_RANGE_NAME = 'scanner_sheet!A2:F'
+SPREADSHEET_ID = '1IMZrcDvgddjarKStUBvvv3S-6NIrVxBSd_fl4yAMP2g'
+RANGE_NAME = 'scanner_sheet!A2:F'
 
 def sheetConnect():
     
@@ -47,6 +47,7 @@ def sheetConnect():
     
     return sheet
 
+
 sheet = sheetConnect()
 
 cred = firebase_admin.credentials.Certificate('serviceAccountKey.json')
@@ -58,53 +59,77 @@ beacons_ref = db.reference('localiza/beacons')
 scanner_ref = db.reference('localiza/scanner')
 dict_ref_scanner = db.reference('localiza/dictionary/scanners')
 dict_ref_beacon = db.reference('localiza/dictionary/beacons')
+update_ref = db.reference('localiza/update_status')
 
 dict_data_scanner = dict_ref_scanner.get()
 dict_data_beacon = dict_ref_beacon.get()
 
-current_time = datetime.now()
-
 scanner_ref.delete()
 
-result = sheet.values().clear(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME).execute()
+result = sheet.values().clear(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
 
-#por beacon
-for beacon_key in beacons_ref.get().keys():
-    beacon_data = beacons_ref.child(beacon_key).get()
-    lowest_rssi_value = int(-100)
-    lowest_rssi_time = int()
-    lowest_rssi_device = str()
-    aux = False
+last_filter = datetime.now()
+status_check = False
 
-    #por scanner dentro do beacon
-    for device_key in beacon_data.keys():
-        device_data = beacon_data[device_key]
-        valores_adicionar = []
-        device_time = datetime.fromtimestamp(device_data['time'])
+while(1):
 
-        try:
-            dict_data_beacon[beacon_key]
-            if current_time - device_time > timedelta(minutes=1440):
-                beacons_ref.child(beacon_key).child(device_key).delete()
-                print(f"Deleted {device_key} from {beacon_key}")
+    current_time = datetime.now()
 
-            elif device_data['rssi'] > lowest_rssi_value:
-                lowest_rssi_value = device_data['rssi']
-                lowest_rssi_device = device_key
-                lowest_rssi_time = device_data['time']
-                aux = True
-        except:
-            print("no device in dict")
-            beacons_ref.child(beacon_key).delete()
+    if current_time - last_filter > timedelta(minutes=1):
+            
+        if status_check == False:
 
-    device_time = str(datetime.fromtimestamp(lowest_rssi_time))
-    valores_adicionar.append([dict_data_beacon[beacon_key], beacon_key, lowest_rssi_value, device_time, dict_data_scanner[lowest_rssi_device], lowest_rssi_device])
-    result = sheet.values().append(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME, valueInputOption="RAW", body={"values": valores_adicionar}).execute() 
+            for scanner_status in update_ref.get().keys():
+                status_data = update_ref.child(scanner_status).get()
+            
+                if status_data == False:
+                    print(f"{scanner_status} not available")
+                    last_filter = datetime.now()
+    
+            status_check = True
 
-    if aux:
-        scanner_ref.child(lowest_rssi_device).child(beacon_key).set({
-            'rssi': lowest_rssi_value,
-            'time': lowest_rssi_time
-        })
-        print(f"Added {beacon_key} to {lowest_rssi_device}")
-        
+        else:
+            #por beacon
+            for beacon_key in beacons_ref.get().keys():
+                beacon_data = beacons_ref.child(beacon_key).get()
+                lowest_rssi_value = int(-100)
+                lowest_rssi_time = int()
+                lowest_rssi_device = str()
+                aux = False
+
+                #por scanner dentro do beacon
+                for device_key in beacon_data.keys():
+                    device_data = beacon_data[device_key]
+                    valores_adicionar = []
+                    device_time = datetime.fromtimestamp(device_data['time'])
+
+                    try:
+                        dict_data_beacon[beacon_key]
+                        if current_time - device_time > timedelta(minutes=131400):
+                            beacons_ref.child(beacon_key).child(device_key).delete()
+                            print(f"Deleted {device_key} from {beacon_key}")
+
+                        elif device_data['rssi'] > lowest_rssi_value:
+                            lowest_rssi_value = device_data['rssi']
+                            lowest_rssi_device = device_key
+                            lowest_rssi_time = device_data['time']
+                            aux = True
+                    except:
+                        print("no device in dict")
+                        beacons_ref.child(beacon_key).delete()
+
+                device_time = str(datetime.fromtimestamp(lowest_rssi_time))
+                valores_adicionar.append([dict_data_beacon[beacon_key], beacon_key, lowest_rssi_value, device_time, dict_data_scanner[lowest_rssi_device], lowest_rssi_device])
+                result = sheet.values().append(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME, valueInputOption="RAW", body={"values": valores_adicionar}).execute() 
+
+                if aux:
+                    scanner_ref.child(lowest_rssi_device).child(beacon_key).set({
+                        'rssi': lowest_rssi_value,
+                        'time': lowest_rssi_time
+                    })
+                    print(f"Added {beacon_key} to {lowest_rssi_device}")
+
+            for scanner_status in update_ref.get().keys():
+                update_ref.child(scanner_status).set(False)
+            status_check = False
+            last_filter = datetime.now()
