cafe635b6a84abfdda4e1936b5c5bc75a6658f57
felipera11 <105614828+felipera11@users.noreply.github.com>
2023-08-30 00:12:21 -0300

    refactored python scripts

--- python/main.py
+++ python/main.py
@@ -1,84 +1,59 @@
-from __future__ import print_function
-
 import firebase_admin
 from firebase_admin import db
+
 from datetime import datetime, timedelta
 
-import os.path
+import sheetConnect
+#sensitive data in a separate file for better security
+import sensitiveData
 
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+SPREADSHEET_ID = sensitiveData.SPREADSHEET_ID
+RANGE_NAME = sensitiveData.RANGE_NAME
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+serviceAccountKey = sensitiveData.serviceAccountKey
 
-# The ID and range of a sample spreadsheet.
-SPREADSHEET_ID = '1IMZrcDvgddjarKStUBvvv3S-6NIrVxBSd_fl4yAMP2g'
-RANGE_NAME = 'scanner_sheet!A2:F'
+databaseURL = sensitiveData.databaseURL
 
-def sheetConnect():
-    
-    creds = None
-    
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+#connect to google sheets
+sheet = sheetConnect.sheetConnect()
 
-    try:
-        service = build('sheets', 'v4', credentials=creds)
-
-        # Call the Sheets API
-        sheet = service.spreadsheets()
-       
-    except HttpError as err:
-        print(err)
-    
-    return sheet
-
-
-sheet = sheetConnect()
-
-cred = firebase_admin.credentials.Certificate('serviceAccountKey.json')
+#connect to firebase
+cred = firebase_admin.credentials.Certificate(serviceAccountKey)
 default_app = firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://projeto-localiza-388217-default-rtdb.firebaseio.com/'
+    'databaseURL': databaseURL
 })
 
+#set the references to the database
 beacons_ref = db.reference('localiza/beacons')
 scanner_ref = db.reference('localiza/scanner')
 dict_ref_scanner = db.reference('localiza/dictionary/scanners')
 dict_ref_beacon = db.reference('localiza/dictionary/beacons')
 update_ref = db.reference('localiza/update_status')
 
+#set the dictionaries for the beacons and scanners
 dict_data_scanner = dict_ref_scanner.get()
 dict_data_beacon = dict_ref_beacon.get()
 
+#clear the database
 scanner_ref.delete()
 
+#clear the spreadsheet
 result = sheet.values().clear(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
 
 last_filter = datetime.now()
 status_check = False
 
+#main loop
 while(1):
-
+    #get the current time
     current_time = datetime.now()
 
+    #check if the last filter was more than 1 minute ago
     if current_time - last_filter > timedelta(minutes=1):
-            
-        if status_check == False:
 
+        #check if the status check was already done 
+        if status_check == False:
+            #status check
             for scanner_status in update_ref.get().keys():
                 status_data = update_ref.child(scanner_status).get()
             
@@ -88,8 +63,10 @@ while(1):
     
             status_check = True
 
+        #if the status check was already done
         else:
-            #por beacon
+
+            #iterating through the beacons
             for beacon_key in beacons_ref.get().keys():
                 beacon_data = beacons_ref.child(beacon_key).get()
                 lowest_rssi_value = int(-100)
@@ -97,12 +74,13 @@ while(1):
                 lowest_rssi_device = str()
                 aux = False
 
-                #por scanner dentro do beacon
+                #iterating through the scanners
                 for device_key in beacon_data.keys():
                     device_data = beacon_data[device_key]
                     valores_adicionar = []
                     device_time = datetime.fromtimestamp(device_data['time'])
 
+                    #check if the device is in the dictionary
                     try:
                         dict_data_beacon[beacon_key]
                         if current_time - device_time > timedelta(minutes=131400):
@@ -114,21 +92,25 @@ while(1):
                             lowest_rssi_device = device_key
                             lowest_rssi_time = device_data['time']
                             aux = True
+                    #if the device is not in the dictionary, delete it
                     except:
                         print("no device in dict")
                         beacons_ref.child(beacon_key).delete()
 
+                #add the data to the spreadsheet
                 device_time = str(datetime.fromtimestamp(lowest_rssi_time))
                 valores_adicionar.append([dict_data_beacon[beacon_key], beacon_key, lowest_rssi_value, device_time, dict_data_scanner[lowest_rssi_device], lowest_rssi_device])
                 result = sheet.values().append(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME, valueInputOption="RAW", body={"values": valores_adicionar}).execute() 
 
+                #add the data to the database
                 if aux:
                     scanner_ref.child(lowest_rssi_device).child(beacon_key).set({
                         'rssi': lowest_rssi_value,
                         'time': lowest_rssi_time
                     })
                     print(f"Added {beacon_key} to {lowest_rssi_device}")
-
+                    
+            #reset the status check
             for scanner_status in update_ref.get().keys():
                 update_ref.child(scanner_status).set(False)
             status_check = False
